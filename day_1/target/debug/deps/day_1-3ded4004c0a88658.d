903d7882fb74169c99faeb1f693f524bc824fa4d
EsinShadrach <esinshadrachrafe@gmail.com>
2024-12-01 21:12:38 +0100

    git ignore

--- day_1/target/debug/deps/day_1-3ded4004c0a88658.d
+++
@@ -1,5 +0,0 @@
-/Users/mac/codes/playground/advent-of-code-2024/day_1/target/debug/deps/libday_1-3ded4004c0a88658.rmeta: src/main.rs
-
-/Users/mac/codes/playground/advent-of-code-2024/day_1/target/debug/deps/day_1-3ded4004c0a88658.d: src/main.rs
-
-src/main.rs:
