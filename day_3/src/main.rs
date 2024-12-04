e72e4799b08bb348b2118ca2d55742ef004ffebc
EsinShadrach <esinshadrachrafe@gmail.com>
2024-12-04 06:46:40 +0100

    Finished day 3 part 1

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -1,3 +1,28 @@
+use regex::Regex;
+
 fn main() {
     println!("Hello, world!");
+    read_input_txt();
+}
+
+fn read_input_txt() {
+    const INPUT_FILE: &str = "input.txt";
+
+    let input = std::fs::read_to_string(INPUT_FILE).unwrap();
+    // match mul(digit-X, digit-Y)
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    let mut result = 0;
+
+    for line in input.lines() {
+        for cap in re.captures_iter(line) {
+            let x: i32 = cap[1].parse().unwrap();
+            let y: i32 = cap[2].parse().unwrap();
+            let multiplication = x * y;
+
+            println!("Found mul({}, {}), result: {}", x, y, multiplication);
+            result += x * y;
+        }
+    }
+
+    println!("Total result: {}", result);
 }
